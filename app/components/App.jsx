8537fc4597523ea5aff59baf71000bc2a37ddbc7
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-06-27 18:14:02 +0300

    Components added styles

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -28,7 +28,8 @@ export default class App extends React.Component {
         return (
             <div>
                 {/*<button onClick={() => console.log('add note')}>+</button>*/}
-                <button onClick={this.addNote}>+</button>
+
+                <button className="add-note" onClick={this.addNote}>+</button>
 
                 <Notes
                     notes={notes}
